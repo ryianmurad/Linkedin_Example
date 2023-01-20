78228156afb680b2f1e1d50ba688c5890c6b2145
ryianmurad <ryian_fattahillah_murad@teknokrat.ac.id>
2023-01-20 14:54:46 +0700

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,13 +11,17 @@ function Sidebar() {
           <img src={SidebarImg} alt="" />
           <Avatar className='sidebar_avatar' src={Myname}/>
           <h2>My Name</h2>
-          <h4>zzzzzzzz@example.com</h4>
+          <h4>my_name@example.com</h4>
         </div>
 
         <div className="sidebar_stats">
-          <div className="sidebar_stats">
-            <p>bla bla bla</p>
-            <p>nasaksasj</p>
+          <div className="sidebar_stat">
+            <p>Viewed you</p>
+            <p className='sidebar_statNumber'>1,3453</p>
+          </div>
+          <div className="sidebar_stat">
+            <p>Following you</p>
+            <p className='sidebar_statNumber'>532</p>
           </div>
         </div>
 
