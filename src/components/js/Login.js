f0796ac9caa666b6d5c55d21b04a1284dd38de3b
ryianmurad <ryian_fattahillah_murad@teknokrat.ac.id>
2023-01-21 16:16:10 +0700

    login done 4:15 PM

--- src/components/js/Login.js
+++ src/components/js/Login.js
@@ -8,10 +8,23 @@ const Login = () => {
                 <a href='/'>
                     <img src="/images/login-logo.svg" alt="" />
                 </a>
-                <div className='login_container'>
-                    <a href="/home">Join now</a>
+                <div className='login_section'>
+                    <a className='joinNow' href="/home">Join now</a>
+                    <a className='signIn' href="">Sign in</a>
                 </div>
             </nav>
+            <section className='section'>
+                <div className='hero'>
+                    <h1>Welcome to your professional community</h1>
+                    <img src="/images/login-hero.svg" alt="" />
+                </div>
+                <div className='form'>
+                    <button className='button'>
+                        <img src="/images/google.svg" alt="" />
+                        Sign in with Google
+                    </button>
+                </div>
+            </section>
         </div>
     )
 }
